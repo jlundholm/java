faaf052d8742a30d21fa93b4b1c348e57bdc53d9
Jared Lundholm <lundholmj@gmail.com>
2022-11-18 11:22:36 -0700

    created ByteShortIntLong

--- HelloWorld2/src/Hello.java
+++ HelloWorld2/src/Hello.java
@@ -1,9 +1,17 @@
+import java.sql.SQLOutput;
+
 public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, Jared");
 
         int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int sum = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(sum);
+
+        int myLastOne = 1000 - sum;
+        System.out.println(myLastOne);
     }
 }
