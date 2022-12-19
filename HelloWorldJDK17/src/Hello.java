fc57563d56c9b0815ca795587ff30c8e08bdac40
Jared Lundholm <lundholmj@gmail.com>
2022-12-19 12:50:41 -0700

    added IF statement

--- HelloWorldJDK17/src/Hello.java
+++ HelloWorldJDK17/src/Hello.java
@@ -2,6 +2,10 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, Jared");
-        System.out.print("Hello World");
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
