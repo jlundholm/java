59722995e6fe1d12f37360426721fa9c5e2e1d07
Jared Lundholm <lundholmj@gmail.com>
2022-12-22 10:50:09 -0700

    FirstMethodChallenge

---
+++ FirstMethodChallenge/src/MethodChallenge.java
@@ -0,0 +1,38 @@
+public class MethodChallenge {
+    public static void main(String[] args) {
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Jared", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Brooke", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Elly", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Ashlyn", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("Mickie", highScorePosition);
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+        + highScorePosition + " on the high score list.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        int position = 4;
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
+        }
+        return position;
+    }
+
+}
