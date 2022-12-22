aa9db1ed4e4b43fd4f82dfa1a15f05ccdef46b11
Jared Lundholm <lundholmj@gmail.com>
2022-12-22 09:06:57 -0700

    Methods in Java

--- ifKeywordAndCodeBlocks/src/MainChallenge.java
+++ ifKeywordAndCodeBlocks/src/MainChallenge.java
@@ -1,26 +1,21 @@
 public class MainChallenge {
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+
+        calculateScore(true, 800, 5, 100);
+
+        calculateScore(true, 10000, 8, 200);
+
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         int finalScore = score;
 
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        finalScore = score;
-
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("your new final score was " + finalScore);
-        }
-
     }
+
 }
