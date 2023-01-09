947bc6ff150572455d51a19d9e259746ed1f3702
Jared Lundholm <lundholmj@gmail.com>
2023-01-09 14:27:09 -0700

    finished SecondsAndMinutesChallenge

--- SecondsAndMinutesChallenge/src/Main.java
+++ SecondsAndMinutesChallenge/src/Main.java
@@ -2,22 +2,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(3701));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65,45));
+        System.out.println(getDurationString(65, 145));
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
     }
 
     public static String getDurationString(int seconds) {
-        int hours = 0;
-        int minutes = 0;
-        int remainingSeconds = 0;
-        hours = (seconds / 60) / 60;
-        minutes = seconds % 60;
-        remainingSeconds = seconds % 60;
+        if (seconds < 0) {
+            return "Invalid data for seconds(" + seconds
+                    + "), must be a positive integer value";
+        }
 
-        return hours + "h " + minutes + "m " + remainingSeconds + "s' ";
+        return getDurationString(seconds / 60, seconds % 60);
     }
 
     public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0) {
+            return "Invalid data for minutes(" + minutes
+                    + "), must be a positive integer value";
+        }
+        if (seconds <0 || seconds >= 59){
+            return "Invalid data for seconds(" + seconds
+                    + "), must be between 0 and 59";
+        }
+        int hours = minutes / 60;
 
-        return "";
+        int remainingMinutes = minutes % 60;
+
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
 }
